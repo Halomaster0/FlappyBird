59daf416ca6f6f42d6f7c7e375897f6f970016c0
Halomaster0 <Halomaster2006lol@gmail.com>
2023-01-14 20:53:32 -0500

    inital commit

---
+++ Assets/Scrpits/GameManager.cs
@@ -0,0 +1,91 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public Player player;
+
+    private int score;
+
+    public Text scoreText;
+
+    public GameObject playButton;
+
+    public GameObject gameOver;
+
+    private int highScore;
+    //number of plays
+    private int nop = 0;
+
+    private int top;
+
+    public Text highscoreText;
+    private void Awake()
+    {
+
+        Application.targetFrameRate = 60;
+        Pause();
+
+    }
+
+    private void Start()
+    {   
+        if(nop == 0)
+        {
+           highscoreText.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+        } 
+
+    }
+    public void Play()
+    {
+        nop++;
+        score = 0;
+        scoreText.text = score.ToString();
+
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        Pipes[] pipes = FindObjectsOfType<Pipes>();
+        for(int i = 0; i < pipes.Length; i++)
+        {
+            Destroy(pipes[i].gameObject);
+        }
+
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+
+    }
+    public void GameOver()
+    {
+        HighScore();
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+        
+        Pause();
+    }
+    public void IncreaseScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+   
+   
+   void HighScore()
+    {
+        if (score > PlayerPrefs.GetInt("HighScore", 0))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            highscoreText.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+        }
+
+
+    }
+
+}
