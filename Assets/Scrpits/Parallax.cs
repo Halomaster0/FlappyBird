6d02b88ebf8b38f9c4ac070a4c5fe30f46166733
Halomaster0 <122708196+Halomaster0@users.noreply.github.com>
2023-01-24 09:58:34 -0500

    Update Parallax.cs

--- Assets/Scrpits/Parallax.cs
+++ Assets/Scrpits/Parallax.cs
@@ -1,18 +1,23 @@
-
 using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
+    // Reference to the MeshRenderer component
     private MeshRenderer meshRenderer;
+    // Variable to control the animation speed
     public float animationSpeed  = 1f; 
+
+    // Function that runs when the script is first enabled
     private void Awake()
     {
+        // Get the MeshRenderer component
         meshRenderer = GetComponent<MeshRenderer>();
-            
     }
 
+    // Function that runs every frame
     private void Update()
     {
+        // Offset the main texture of the material attached to the MeshRenderer by the animation speed multiplied by Time.deltaTime
         meshRenderer.material.mainTextureOffset += new Vector2(animationSpeed * Time.deltaTime, 0);
     }
 }
