64d31521bd423ec80f0e53cf1a9445023b2dbe7e
Halomaster0 <122708196+Halomaster0@users.noreply.github.com>
2023-01-24 10:06:30 -0500

    Update Pipes.cs

--- Assets/Scrpits/Pipes.cs
+++ Assets/Scrpits/Pipes.cs
@@ -1,22 +1,29 @@
-
 using UnityEngine;
 
 public class Pipes : MonoBehaviour
 {
+    // Variable for controlling the speed of the pipes
     public float speed = 5f;
+    // Variable to store the left edge of the screen
     private float leftEdge;
 
+    // Function that runs on the first frame
     private void Start()
     {
+        // Get the left edge of the screen in world coordinates and subtract 1 to account for offscreen space
         leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 1f;
     }
+
+    // Function that runs every frame
     private void Update()
     {
-
+        // Move the pipes to the left based on the speed and time since the last frame
         transform.position += Vector3.left * speed * Time.deltaTime;
 
+        // Check if the pipes have moved past the left edge of the screen
         if(transform.position.x < leftEdge)
         {
+            // If so, destroy the pipes game object
             Destroy(gameObject);
         }
     }
