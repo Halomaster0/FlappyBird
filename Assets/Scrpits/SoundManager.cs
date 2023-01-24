0ab07a73b9599dfada37facb15c7d11876856429
Halomaster0 <122708196+Halomaster0@users.noreply.github.com>
2023-01-24 10:17:41 -0500

    Update SoundManager.cs

--- Assets/Scrpits/SoundManager.cs
+++ Assets/Scrpits/SoundManager.cs
@@ -4,36 +4,45 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    // Function to play the jump sound
     public static void jump()
     {
+        // Create a new game object with an AudioSource component
         GameObject gameObject = new GameObject("Sound", typeof(AudioSource));
         AudioSource audiosource = gameObject.GetComponent<AudioSource>();
+        // Play the jump sound from the GameManager script
         audiosource.PlayOneShot(FindObjectOfType<GameManager>().birdJump);
     }
 
+    // Function to play the point sound
     public static void point()
     {
+        // Create a new game object with an AudioSource component
         GameObject gameObject = new GameObject("Sound", typeof(AudioSource));
         AudioSource audiosource = gameObject.GetComponent<AudioSource>();
+        // Play the point sound from the GameManager script
         audiosource.PlayOneShot(FindObjectOfType<GameManager>().collect);
 
     }
 
+    // Function to play the death sound
     public static void death()
     {
+        // Create a new game object with an AudioSource component
         GameObject gameObject = new GameObject("Sound", typeof(AudioSource));
         AudioSource audiosource = gameObject.GetComponent<AudioSource>();
+        // Play the death sound from the GameManager script
         audiosource.PlayOneShot(FindObjectOfType<GameManager>().dead);
 
     }
 
+    // Function to play the highscore sound
     public static void highScore()
     {
+        // Create a new game object with an AudioSource component
         GameObject gameObject = new GameObject("Sound", typeof(AudioSource));
         AudioSource audiosource = gameObject.GetComponent<AudioSource>();
-        audiosource.PlayOneShot(FindObjectOfType<GameManager>().highscore);
-        
-    }
-
-
+        // Play the highscore sound from the GameManager script
+	    audiosource.PlayOneShot(FindObjectOfType<GameManager>().highscore);
+}
 }
